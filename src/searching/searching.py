2128a3ef87b343ea655438b143b8548ebeb681ec
FreedomWriter <natalienetworks@gmail.com>
2020-08-09 18:40:47 -0500

    searching stretch

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -54,6 +54,28 @@ print(binary_search(arr2, 0, 0, len(arr2)-1))
 # You can implement this function either recursively 
 # or iteratively
 def agnostic_binary_search(arr, target):
+    # if len(arr) == 0:
+    #     return -1
+    
     # Your code here
-    pass
+    start = 0
+    middle = 0
+    end = len(arr) -1
+
+
+
+    while start <= end:
+        middle = start + end // 2
+        try:
+            if arr[middle] > target:
+                if arr[middle] > arr[middle -1]:
+                    return binary_search(arr, target, start, middle-1)
+                else:
+                    return binary_search(arr, target,  middle-1, end)
+            else:
+                return arr.index(target)
+        except:
+            return -1
+        
+    return -1  # not found  
 
